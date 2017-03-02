ec3b6372d0bb69bbd693fbde910d3e982bd5fd2a
MrFlorius <vadim.tsvetkov80@gmail.com>
2017-03-03 00:50:55 +0300

    Changes to be committed: 	new file:   JS_Prog/.vs/JS_Prog/v14/.suo 	new file:   JS_Prog/Data/le-1.txt 	new file:   JS_Prog/Data/map.jpg 	new file:   JS_Prog/Data/msm.txt 	new file:   "JS_Prog/Data/\320\233\321\203\320\274_\320\277\321\200\320\270\320\274\320\265\321\200.docx" 	new file:   "JS_Prog/Data/\320\233\321\203\320\274_\321\210\320\260\320\261\320\273\320\276\320\275.docx" 	new file:   "JS_Prog/Data/\320\242\320\265\321\205\320\275\320\270\321\207\320\265\321\201\320\272\320\276\320\265 \320\267\320\260\320\264\320\260\320\275\320\270\320\265 \320\277\321\200\320\276\320\263\321\200\320\260\320\274\320\274\320\275\321\213\320\265 \321\200\320\265\321\210\320\265\320\275\320\270\321\217 \320\264\320\273\321\217 \320\261\320\270\320\267\320\275\320\265\321\201\320\260 \320\274\320\260\321\200\321\202 2017.docx" 	new file:   JS_Prog/JS_Prog.sln 	new file:   JS_Prog/JS_Prog.v11.suo 	new file:   JS_Prog/JS_Prog/App.config 	new file:   JS_Prog/JS_Prog/Form1.Designer.cs 	new file:   JS_Prog/JS_Prog/Form1.cs 	new file:   JS_Prog/JS_Prog/Form1.resx 	new file:   JS_Prog/JS_Prog/Graph.cs 	new file:   JS_Prog/JS_Prog/JS_Prog.csproj 	new file:   JS_Prog/JS_Prog/Program.cs 	new file:   JS_Prog/JS_Prog/Properties/AssemblyInfo.cs 	new file:   JS_Prog/JS_Prog/Properties/Resources.Designer.cs 	new file:   JS_Prog/JS_Prog/Properties/Resources.resx 	new file:   JS_Prog/JS_Prog/Properties/Settings.Designer.cs 	new file:   JS_Prog/JS_Prog/Properties/Settings.settings 	new file:   JS_Prog/JS_Prog/bin/Debug/JS_Prog.exe 	new file:   JS_Prog/JS_Prog/bin/Debug/JS_Prog.exe.config 	new file:   JS_Prog/JS_Prog/bin/Debug/JS_Prog.pdb 	new file:   JS_Prog/JS_Prog/bin/Debug/JS_Prog.vshost.exe 	new file:   JS_Prog/JS_Prog/bin/Debug/JS_Prog.vshost.exe.config 	new file:   JS_Prog/JS_Prog/bin/Debug/JS_Prog.vshost.exe.manifest 	new file:   JS_Prog/JS_Prog/obj/Debug/DesignTimeResolveAssemblyReferences.cache 	new file:   JS_Prog/JS_Prog/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   JS_Prog/JS_Prog/obj/Debug/JS_Prog.Form1.resources 	new file:   JS_Prog/JS_Prog/obj/Debug/JS_Prog.Properties.Resources.resources 	new file:   JS_Prog/JS_Prog/obj/Debug/JS_Prog.csproj.FileListAbsolute.txt 	new file:   JS_Prog/JS_Prog/obj/Debug/JS_Prog.csproj.GenerateResource.Cache 	new file:   JS_Prog/JS_Prog/obj/Debug/JS_Prog.csprojResolveAssemblyReference.cache 	new file:   JS_Prog/JS_Prog/obj/Debug/JS_Prog.exe 	new file:   JS_Prog/JS_Prog/obj/Debug/JS_Prog.pdb 	new file:   JS_Prog/JS_Prog/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   JS_Prog/JS_Prog/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   JS_Prog/JS_Prog/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs

---
+++ JS_Prog/JS_Prog/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace JS_Prog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static bool NextPermutation<T>(IList<T> a) where T : IComparable<T>
+        {
+            if (a.Count < 2) return false;
+            var k = a.Count - 2;
+
+            while (k >= 0 && a[k].CompareTo(a[k + 1]) >= 0) k--;
+            if (k < 0) return false;
+
+            var l = a.Count - 1;
+            while (l > k && a[l].CompareTo(a[k]) <= 0) l--;
+
+            var tmp = a[k];
+            a[k] = a[l];
+            a[l] = tmp;
+
+            var i = k + 1;
+            var j = a.Count - 1;
+            while (i < j)
+            {
+                tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+
+        private static List<List<T>> Premutations<T>(IList<T> a) where T : IComparable<T>
+        {
+            var l = new List<List<T>>();
+            do
+            {
+                l.Add(a.ToList());
+            } while (NextPermutation(a));
+            return l;
+        }
+
+        public static List<List<T>> GetAllCombos<T>(List<T> list)
+        {
+            var comboCount = (int) Math.Pow(2, list.Count) - 1;
+            var result = new List<List<T>>();
+            for (var i = 1; i < comboCount + 1; i++)
+            {
+                // make each combo here
+                result.Add(new List<T>());
+                for (var j = 0; j < list.Count; j++)
+                    if ((i >> j) % 2 != 0)
+                        result.Last().Add(list[j]);
+            }
+            return result;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graph g = new Graph("../../../Data/le-1.txt");
+            g.Matrix("../../../Data/msm.txt");
+            var a = g.Dijkstra(0)[1];
+            var b = g.Path(a, 5);
+            var clients_raw = Enumerable.Range(0, 9).ToArray();
+            var clients = AllCombinations.Combinations(Enumerable.Range(0, 9).ToArray()).SelectMany(x => Premutations(x)).ToList().OrderBy(x => x[0]);
+            List<int> distances = new List<int>();
+            foreach (var combination in clients)
+            {
+                
+            }
+            var s = clients.Aggregate("", (current, x) => current + (string.Join(", ", x) + "\t"));
+            MessageBox.Show(s);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+
+
+    internal class AllCombinations
+    {
+        public static List<List<int>> Combinations(int[] array, int startingIndex = 0, int combinationLenght = 2)
+        {
+            var combinations = new List<List<int>>();
+            if (combinationLenght == 2)
+            {
+                var combinationsListIndex = 0;
+                for (var arrayIndex = startingIndex; arrayIndex < array.Length; arrayIndex++)
+                for (var i = arrayIndex + 1; i < array.Length; i++)
+                {
+                    //add new List in the list to hold the new combination
+                    combinations.Add(new List<int>());
+
+                    //add the starting index element from “array”
+                    combinations[combinationsListIndex].Add(array[arrayIndex]);
+                    while (combinations[combinationsListIndex].Count < combinationLenght)
+                        combinations[combinationsListIndex].Add(array[i]);
+                    combinationsListIndex++;
+                }
+
+                return combinations;
+            }
+
+            var combinationsofMore = new List<List<int>>();
+            for (var i = startingIndex; i < array.Length - combinationLenght + 1; i++)
+            {
+                //generate combinations of lenght-1(if lenght > 2 we enter into recursion)
+                combinations = Combinations(array, i + 1, combinationLenght - 1);
+
+                //add the starting index Elemetn in the begginnig of each newly generated list
+                for (var index = 0; index < combinations.Count; index++)
+                    combinations[index].Insert(0, array[i]);
+
+                for (var y = 0; y < combinations.Count; y++)
+                    combinationsofMore.Add(combinations[y]);
+            }
+
+            return combinationsofMore;
+        }
+    }
+}
---
+++ JS_Prog/JS_Prog/Graph.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace JS_Prog
+{
+    internal class Graph
+    {
+        public List<int[]> Edges;
+        public int[,] Mat;
+        private bool IsChanged = true;
+        private List<int> _path = new List<int>();
+
+        public Graph(string path)
+        {
+            StreamReader r = new StreamReader(path);
+            Edges = Regex.Split(r.ReadToEnd(), "\r\n").ToList().Select(s => s.Split().Select( i => int.Parse(i) - 1).ToArray()).ToList();
+            this.Matrix();
+        }
+
+        public void AddEdge(int point1, int point2, int weight)
+        {
+            Edges.Add(new int[] { point1, point2, weight });
+            Edges.Add(new int[] { point2, point1, weight });
+            IsChanged = true;
+        }
+
+        public void AddDirectEdge(int from, int to, int weight)
+        {
+            Edges.Add(new int[] { from, to, weight });
+            IsChanged = true;
+        }
+
+        public void Matrix()
+        {
+            if (IsChanged)
+            {
+                int size = Edges.OrderBy(s => s.Take(2).Max()).ToList()[Edges.Count - 1].Take(2).Max() + 1;
+                Mat = new int[size, size];
+                foreach (var edge in Edges) Mat[edge[0], edge[1]] = edge[2];
+            }
+        }
+
+        internal void Matrix(string path)
+        {
+            this.Matrix();
+            StreamWriter w = new StreamWriter(path);
+            w.Write(this.ToString());
+            w.Close();
+        }
+
+        public override string ToString()
+        {
+            int rowLength = Mat.GetLength(0);
+            int colLength = Mat.GetLength(1);
+            string s = "";
+            int tabulate = Mat.Cast<int>().Max().ToString().Length;
+
+            for (int i = 0; i < rowLength; i++){
+                for (int j = 0; j < colLength; j++)
+                    s += string.Format(string.Format("{{0, {0}}}", tabulate + 1), Mat[i, j]);
+                s += (Environment.NewLine + Environment.NewLine);
+            }
+            return s;
+        }
+
+        public int MinDistance(int[] dist, bool[] queue)
+        {
+            int min = int.MaxValue;
+            int min_idx = -1;
+            for(int i = 0; i < dist.Length; i++)
+            {
+                if((dist[i] < min) && (!queue[i])) 
+                {
+                    min = dist[i];
+                    min_idx = i;
+                }
+            }
+            return min_idx;
+        }
+
+        private int _Path(IReadOnlyList<int> parent, int j)
+        {
+            _path.Add(j);
+            if (parent[j] == -1)
+            {
+                _path.Add(j);
+                return j;
+            }
+            return _Path(parent, parent[j]);;
+            //path.Add(j);
+        }
+
+        public List<int> Path(int[] parent, int j)
+        {
+            _Path(parent, j);
+            _path = _path.Take(count: _path.Count - 1).Reverse().ToList();
+            return _path;
+        }
+
+        public int[][] Dijkstra(int start)
+        { 
+            int n = Mat.GetLength(0);
+            int[] dist = Enumerable.Repeat<int>(int.MaxValue, n).ToArray();
+            dist[start] = 0;
+            int[] parent = Enumerable.Repeat<int>(-1, n).ToArray();
+            bool[] queue = new bool[n];
+
+            while (queue.Contains(false))
+            {
+                int u = MinDistance(dist, queue);
+                queue[u] = true;
+                for (int i = 0; i < n; i++)
+                {
+                    if ((Mat[u,i] != 0) && (!queue[i]))
+                        if (dist[u] + Mat[u, i] < dist[i])
+                        {
+                            dist[i] = dist[u] + Mat[u, i];
+                            parent[i] = u;
+                        }
+                }
+            }
+            int [][] ret = new int[2][];
+            ret[0] = dist;
+            ret[1] = parent;
+            return ret;
+        }
+
+        public List<int[][]> DistanceMatrix()
+        {
+            List<int[][]> ret = new List<int[][]>();
+            for (int i = 0; i < Mat.GetLength(0); i++)
+            {
+                ret.Add(Dijkstra(i));
+            }
+            return ret;
+        }
+    }
+}
